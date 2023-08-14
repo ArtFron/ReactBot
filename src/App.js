9567939138b18e4aa610e2bc7b3b6413e0c0d924
Bessartik <Bessonovarti@yandex.ru>
2023-08-14 08:25:49 +0300

    React Bot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,81 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
+import ChatBot from 'react-simple-chatbot';
+import { ThemeProvider } from 'styled-components';
 function App() {
+  const theme = {
+    background: '#FFFFE0',
+    fontFamily: 'Kanit, cursive',
+    headerBgColor: '#696969',
+    headerFontColor: '#fff',
+    headerFontSize: '15px',
+    botBubbleColor: '#696969',
+    botFontColor: '#fff',
+    userBubbleColor: 'SlateGrey',
+    userFontColor: '#fff',
+  };
+
+  const steps = [
+    {
+      id: 'Start',
+      message: 'Привет! Что я могу для Вас сделать?',
+      trigger: 'Issues',
+    },
+    {
+      id: 'Issues',
+      options: [
+        {
+          value: 'Pizza',
+          label: 'Заказать пиццу',
+          trigger: 'Pizza',
+        },
+        {
+          value: 'Time',
+          label: 'Установить будильник',
+          trigger: 'Time',
+        },
+        {
+          value: 'Weather',
+          label: 'Вывести погоду',
+          trigger: 'Weather',
+        },
+        {
+          value: 'End',
+          label: 'Закончить работу',
+          trigger: 'End',
+        },
+      ],
+    },
+    {
+      id: 'Pizza',
+      message: 'Хорошо, я закажу пиццу 🍕. Что еще могу сделать?',
+      trigger: 'Issues',
+    },
+    {
+      id: 'Time',
+      message: 'Будильник поставил. Что еще могу сделать?',
+      trigger: 'Issues',
+    },
+    {
+      id: 'Weather',
+      message: 'Вывожу погоду. Что еще могу сделать?',
+      trigger: 'Issues',
+    },
+    {
+      id: 'End',
+      message: 'Прекращаю свою работу',
+      end: true,
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div floated="right">
+      <ThemeProvider theme={theme}>
+        <ChatBot
+          botAvatar="https://sun9-5.userapi.com/impf/c305112/v305112241/3c14/5K6npQI6GN4.jpg?size=1280x1920&quality=96&sign=4f01bb590920863294c68a8142c83a6c&c_uniq_tag=4WgON4837LodJTqic_SaUEwa4va4r9gG_OBrbBDjXo0&type=album"
+          steps={steps}
+        />
+      </ThemeProvider>
     </div>
   );
 }
